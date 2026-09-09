9992f1bf497409b62ba2f9c7e3f2883305dd05dc
Pimvdbos <pimvdb@ziggo.nl>
2026-09-09 13:33:26 +0200

    Add package exports

---
+++ src/ml_portfolio/__init__.py
@@ -0,0 +1,3 @@
+from .backtest import BacktestConfig, load_prices, log_returns_from_prices, run_walk_forward, performance_summary
+
+__all__ = ["BacktestConfig", "load_prices", "log_returns_from_prices", "run_walk_forward", "performance_summary"]
